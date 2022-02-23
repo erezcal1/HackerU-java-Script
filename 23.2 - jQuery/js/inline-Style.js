f6991a1caecaa570d8e672526bd6d2e52bd21414
Erez Calderon <erezcal1@gmail.com>
2022-02-23 12:11:39 +0200

    inline style

---
+++ 23.2 - jQuery/js/inline-Style.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  //for 1 property
+  //css - (normal css property, value)
+  // $("#txt").css("background-color", "green");
+
+  //for multiple properties
+  //passing a object
+  $("#txt").css({ "background-color": "green", color: "red" });
+});
