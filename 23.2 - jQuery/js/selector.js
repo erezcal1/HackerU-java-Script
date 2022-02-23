56b175e42b44b99fc9851bcc6c4100b52f8bb3bd
Erez Calderon <erezcal1@gmail.com>
2022-02-23 09:53:54 +0200

    first init

--- 23.2 - jQuery/js/selector.js
+++ 23.2 - jQuery/js/selector.js
@@ -1,7 +1,13 @@
-//get element by id
-document.getElementById("id");
+//id only
+// document.getElementById("id");
 
 //get element like css do
-document.querySelector("id", name, tagname, "class");
+// document.querySelector("id", name, tagname, "class");
 
-$;
+//get element like css do
+// $("id", name, tagname, "class");
+
+//js
+document.getElementById("btn").addEventListener("click", () => {
+  document.body.style.backgroundColor = "red";
+});
