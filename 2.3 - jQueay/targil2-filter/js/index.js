9e10618595783bd8f6c41566c9436d6c9c283e9c
Erez Calderon <erezcal1@gmail.com>
2022-03-02 14:29:16 +0200

    ikea Targil

--- 2.3 - jQueay/targil2-filter/js/index.js
+++ 2.3 - jQueay/targil2-filter/js/index.js
@@ -1,4 +1,16 @@
 const petsArray = ["dog", "cat", "fish", "mouse", "dodo"];
+const ikeaArray = [
+  "chair red",
+  "table red",
+  "sofa black",
+  "bed",
+  "desk",
+  "chair blue",
+  "table blue",
+  "sofa blue",
+  "bed blue",
+  "desk blue",
+];
 $(document).ready(() => {
   //on "input" event will happen every time the user modifies the input
   $("#text").on("input", () => {
@@ -14,6 +26,18 @@ $(document).ready(() => {
     //add the filtered array to the list
     for (let pet of filterArr) {
       l_O_Animals.append(`<li class="list-group-item">${pet}</li>`);
+    }
+  });
+
+  $("#ikea").on("input", () => {
+    let ikea_Search = $("#ikea").val();
+    let filter_Ikea = ikeaArray.filter((ikea) => {
+      return ikea.includes(ikea_Search);
+    });
+    let l_O_Ikea = $("#list-of-ikea");
+    l_O_Ikea.empty();
+    for (let ikea of filter_Ikea) {
+      l_O_Ikea.append(`<li class="list-group-item">${ikea}</li>`);
     }
   });
 });
