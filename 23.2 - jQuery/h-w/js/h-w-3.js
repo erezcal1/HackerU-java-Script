56c29a69a0f6564c36ab6bd5562c3d65c97f7197
Erez Calderon <erezcal1@gmail.com>
2022-03-07 13:21:53 +0200

    init

--- 23.2 - jQuery/h-w/js/h-w-3.js
+++ 23.2 - jQuery/h-w/js/h-w-3.js
@@ -1,13 +1,45 @@
 let arr = ["Lion", "Tiger", "Elephant", "Giraffe"];
 $(document).ready(() => {
-  for (let i = 0; i < arr.length; i++) {
-    $("#animalList").append(`<li><button>${arr[i]}</button></li>`);
+  let i = 0;
+  for (; i < arr.length; i++) {
+    $("#animalList").append(
+      `<li class="li"><button id="${i}">${arr[i]}</button></li>`
+    );
   }
 
   $("#btn").click(() => {
     let animalName = $("#animal");
     if (animalName.val() != "") {
-      $("#animalList").append(`<li><button>${animalName.val()}</button></li>`);
+      $("#animalList").append(
+        `<li class="li"><button id="${i}">${animalName.val()}</button></li>`
+      );
     }
+    i++;
+  });
+
+  $("#my_Form").on("click", "button", function (e) {
+    let index = $(this).attr("id");
+
+    $(".container").css("display", "block");
+
+    $(".close").click(() => {
+      $(".container").css("display", "none");
+    });
+
+    $("#text").blur(() => {
+      $("#animalList")
+        .children()
+        .eq(index)
+        .html(`<button id="${index}">` + $("#text").val() + `</button>`);
+      console.log(index);
+    });
+
+    $("#remove").click(() => {
+      console.log(index);
+      $("#animalList").children().eq(index).remove();
+      // $(this).closest(".li").remove();
+    });
+
+    e.preventDefault();
   });
 });
